2947bee7e2f6004e0795e20bed146ca5690077dd
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2024-02-26 14:39:59 +0100

    Setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+NAME=flutter_web_test_deploy
+BRANCH=main
+curl -fsSL "https://github.com/rpede/$NAME/archive/refs/heads/$BRANCH.zip" -o $BRANCH.zip
+unzip -nD $BRANCH.zip -x README.md
+mv $NAME-$BRANCH/* $NAME-$BRANCH/.* ./
+rm $BRANCH.zip
+rmdir $NAME-$BRANCH
+flutter pub add 'dev:flutter_test:{"sdk":"flutter"}'  'dev:integration_test:{"sdk":"flutter"}'
